e04d931e9236cfbce7aecb1667a229a0baa33144
Dou Yi <wangjie0806@qq.com>
2025-03-31 20:02:32 +0800

    绑定模板封装成组件

---
+++ src/components/business/template-bind/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div v-if="isShowTemplateSetting">
+    <a-modal v-model:open="isOpenTemplateSetting" title="请选择模板" @ok="setTemplate">
+      <a-form
+        ref="applicationNameRef"
+        :model="formState"
+        :rules="{
+          templateId: [{ required: true, message: '请选择模板', trigger: 'change' }],
+        }"
+        :label-col="{ span: 5 }"
+        :wrapper-col="{ span: 19 }"
+      >
+        <a-form-item label="模板名称" name="templateId">
+          <a-select v-model:value="formState.templateId" placeholder="请选择模板">
+            <a-select-option v-for="item in templateList" :key="item._id" :value="item._id">
+              {{ item.name }}
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+      </a-form>
+    </a-modal>
+    <a-float-button
+      type="primary"
+      shape="square"
+      description="设置模板"
+      @click="isOpenTemplateSetting = true"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import { message } from 'ant-design-vue';
+  import { getExcelTemplateByIds, getExcelTemplateList } from '@/api/backend/api/template';
+  import { publishApplication } from '@/api/backend/api/application';
+  import { saveApplicationData } from '@/api/backend/api/applicationData';
+  import { useUserStore } from '@/store/modules/user';
+
+  defineProps({
+    isShowTemplateSetting: {
+      type: Boolean,
+      default: false,
+    },
+  });
+
+  const emit = defineEmits(['update:modelValue', 'bind-success']);
+
+  const userStore = useUserStore();
+  const isOpenTemplateSetting = ref(false);
+  const applicationNameRef = ref();
+  const formState = ref({
+    templateId: '',
+  });
+  const templateList = ref<any[]>([]);
+
+  const setTemplate = async function () {
+    applicationNameRef.value.validate().then(async () => {
+      bindTemplate(formState.value.templateId);
+    });
+  };
+
+  const bindTemplate = async (id: string) => {
+    getExcelTemplateByIds([id]).then((res) => {
+      const { initDataSource, file } = res[0];
+      return Promise.all([
+        publishApplication({
+          templateId: id,
+          isBuildIn: true,
+          name: '人员信息',
+          icon: 'default',
+          content: file,
+          description: '',
+        }),
+        saveApplicationData({
+          templateId: id,
+          userId: userStore.userInfo.id,
+          applicationData: initDataSource,
+        }),
+      ])
+        .then(() => {
+          message.success('绑定成功');
+          isOpenTemplateSetting.value = false;
+          emit('update:modelValue', false);
+          emit('bind-success');
+        })
+        .catch(() => {
+          message.error('绑定失败');
+        });
+    });
+  };
+
+  const fetchTemplate = async function () {
+    getExcelTemplateList({}).then((res) => {
+      templateList.value = res;
+    });
+  };
+
+  onMounted(() => {
+    fetchTemplate();
+  });
+</script>
--- src/views/human-manage/users/index.vue
+++ src/views/human-manage/users/index.vue
@@ -8,51 +8,10 @@
       @saveWorkBook="saveWorkBook"
       @cellClick="cellClick"
     />
-    <div v-if="isShowTemplateSetting">
-      <a-modal v-model:open="isOpenTemplateSetting" title="请选择模板" @ok="setTemplate">
-        <!-- <a-form
-          :model="formState"
-          ref="applicationNameRef"
-          :wrapper-col="{ span: 20 }"
-          :label-col="{ span: 4 }"
-          :rules="{
-            applicationName: [{ required: true, message: '请选择模板名称', trigger: 'change' }],
-          }"
-        >
-          <a-form-item label="模板名称" name="applicationName">
-            <a-select v-model:value="formState.applicationName" placeholder="请选择模板名称">
-              <a-select-option v-for="item in templateList" :key="item.id" :value="item.id">
-                {{ item.name }}
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-        </a-form> -->
-
-        <a-form
-          ref="applicationNameRef"
-          :model="formState"
-          :rules="{
-            templateId: [{ required: true, message: '请选择模板', trigger: 'change' }],
-          }"
-          :label-col="{ span: 5 }"
-          :wrapper-col="{ span: 19 }"
-        >
-          <a-form-item label="模板名称" name="templateId">
-            <a-select v-model:value="formState.templateId" placeholder="请选择模板">
-              <a-select-option v-for="item in templateList" :key="item._id" :value="item._id">
-                {{ item.name }}
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-        </a-form>
-      </a-modal>
-      <a-float-button
-        type="primary"
-        shape="square"
-        description="设置模板"
-        @click="isOpenTemplateSetting = true"
-      />
-    </div>
+    <template-bind
+      v-model:isShowTemplateSetting="isShowTemplateSetting"
+      @bind-success="fetchExcel"
+    />
   </div>
 </template>
 
@@ -60,21 +19,15 @@
   import { onMounted, ref } from 'vue';
   import { message } from 'ant-design-vue';
   import excelBook from '@/components/business/excel-book/index.vue';
-  import { getApplicationById, publishApplication } from '@/api/backend/api/application';
-  import { getApplicationData, saveApplicationData } from '@/api/backend/api/applicationData';
+  import templateBind from '@/components/business/template-bind/index.vue';
+  import { getApplicationById } from '@/api/backend/api/application';
+  import { getApplicationData } from '@/api/backend/api/applicationData';
   import { getTemplateDataByApplicationName } from '@/api/backend/api/templateData';
-  import { getExcelTemplateList, getExcelTemplateByIds } from '@/api/backend/api/template';
-  import { useUserStore } from '@/store/modules/user';
 
   const APPLICATION_NAME = '人员信息';
   let templateId = '';
-  const userStore = useUserStore();
-  const templateList = ref<any[]>([]);
-  const isShowTemplateSetting = ref(false);
-  const isOpenTemplateSetting = ref(false);
   const excelBookRef = ref();
   const excelBookKey = ref('');
-  const applicationNameRef = ref();
   const content = ref({
     ejs: '',
     dataSource: {
@@ -82,55 +35,9 @@
     },
     fileName: '导出数据文件.xlsx',
   });
-  const formState = ref({
-    templateId: '',
-  });
-
-  const setTemplate = async function () {
-    applicationNameRef.value.validate().then(async () => {
-      console.log('formState', formState.value);
-      bindTemplate(formState.value.templateId);
-    });
-  };
-
-  const bindTemplate = async (id: string) => {
-    getExcelTemplateByIds([id]).then((res) => {
-      const { initDataSource, file } = res[0];
-      return Promise.all([
-        publishApplication({
-          templateId: id,
-          isBuildIn: true,
-          name: '人员信息',
-          icon: 'default',
-          content: file,
-          description: '',
-        }),
-        saveApplicationData({
-          templateId: id,
-          userId: userStore.userInfo.id,
-          applicationData: initDataSource,
-        }),
-      ])
-        .then(() => {
-          message.success('绑定成功');
-          isOpenTemplateSetting.value = false;
-          fetchExcel();
-        })
-        .catch(() => {
-          message.error('绑定失败');
-        });
-    });
-  };
-
+  const isShowTemplateSetting = ref(false);
   const getTemplateId = async function () {
     return getTemplateDataByApplicationName({ applicationName: APPLICATION_NAME });
-  };
-
-  const fetchTemplate = async function () {
-    getExcelTemplateList({}).then((res) => {
-      console.log('res', res);
-      templateList.value = res;
-    });
   };
 
   const fetchExcel = async function () {
@@ -153,6 +60,7 @@
             };
           })
           .catch(() => {
+            message.error('获取模板数据失败');
             isShowTemplateSetting.value = true;
           });
       })
@@ -170,7 +78,6 @@
   };
 
   onMounted(() => {
-    fetchTemplate();
     fetchExcel();
   });
 </script>
