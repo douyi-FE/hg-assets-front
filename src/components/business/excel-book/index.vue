498e280707b8463f8bb3ae9f7b645bbec50d04b9
Gameckh <chenkaihua_kevin@foxmail.com>
2025-07-09 20:56:41 +0800

    修改覆盖导入后无法保存的问题

--- src/components/business/excel-book/index.vue
+++ src/components/business/excel-book/index.vue
@@ -461,7 +461,7 @@
                 sheetData[key].splice(fromRow, rowCount, ...importDataSource);
               } else {
                 // 覆盖替换数据
-                sheetData[key] = importDataSource;
+                sheetData[key].splice(0, sheetData[key].length, ...importDataSource);
               }
             }
           });
